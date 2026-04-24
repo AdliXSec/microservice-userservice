cadfcee971e180ddb9b0e9542a2903403f059dc1
AdliXSec <naufalsyahruradli@gmail.com>
2026-04-25 02:00:38 +0700

    Fix JWWT

--- userservice/app/__init__.py
+++ userservice/app/__init__.py
@@ -1,7 +1,13 @@
+import os
+from datetime import timedelta
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+
+# Muat variabel lingkungan dari file .env
+load_dotenv()
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -10,12 +16,16 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
 
-    # Konfigurasi MySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_db'
+    # Konfigurasi dari Environment Variables (.env)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:@localhost/flask_db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    # Konfigurasi JWT (Ganti secret key ini dengan yang lebih aman nanti)
-    app.config['JWT_SECRET_KEY'] = 'super-secret-key-12345'
+    # Konfigurasi JWT
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'super-secret-key-12345')
+    
+    # Atur durasi token (12 Jam sesuai permintaan)
+    expires_hours = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES_HOURS', 12))
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=expires_hours)
 
     db.init_app(app)
     bcrypt.init_app(app)
