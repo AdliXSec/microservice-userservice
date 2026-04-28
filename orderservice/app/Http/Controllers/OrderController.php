e3a4e85b4f637eabc634f8d4bd328a36d03725b6
AdliXSec <naufalsyahruradli@gmail.com>
2026-04-28 22:16:02 +0700

    change status and add authentication heades to url product service

--- orderservice/app/Http/Controllers/OrderController.php
+++ orderservice/app/Http/Controllers/OrderController.php
@@ -36,12 +36,12 @@ class OrderController extends Controller
     public function index()
     {
         $orders = Order::all();
-        return new OrderResource($orders, 'Success', 'List of orders');
+        return new OrderResource($orders, 'berhasil', 'List of orders');
     }
 
     public function store(Request $request)
     {
-
+        $token = $request->bearerToken();
         $userId = $request->user_id ?? ($request->auth_user['id'] ?? null);
         $userData = $this->fetchUserData($userId);
 
@@ -52,21 +52,23 @@ class OrderController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return new OrderResource(null, 'Failed', $validator->errors());
+            return new OrderResource(null, 'gagal', $validator->errors());
         }
 
         $userResponse = Http::get("http://127.0.0.1:5000/users/{$request->user_id}");
         // $user = $userResponse->json()['data'] ?? null;
 
         if (!$userData) {
-            return new OrderResource(null, 'Failed', 'User not found');
+            return new OrderResource(null, 'gagal', 'User not found');
         }
 
-        $productResponse = Http::get("http://127.0.0.1:8000/api/obat/{$request->product_id}");
+        $productResponse = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->get("http://127.0.0.1:8000/api/obat/{$request->product_id}");
         $productData = $productResponse->json()['data'] ?? null;
 
         if (!$productData) {
-            return new OrderResource(null, 'Failed', 'Product not found');
+            return new OrderResource(null, 'gagal', 'Product not found');
         }
 
         $order = Order::create([
@@ -80,38 +82,44 @@ class OrderController extends Controller
             'status' => 'pending',
         ]);
 
-        Http::put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
-            'stock' => $productData['stock'] - $request->quantity,
-        ]);
+        Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
+                    'stock' => $productData['stock'] - $request->quantity,
+                ]);
 
-        return new OrderResource($order, 'Success', 'Order created successfully');
+        return new OrderResource($order, 'berhasil', 'Order created successfully');
     }
 
     public function show($id)
     {
+        $token = request()->bearerToken();
         $order = Order::find($id);
         if ($order) {
             $data = $order->toArray();
 
             // Get the product details (consume)
-            $productResponse = Http::get("{$this->productServiceUrl}/api/obat/{$order->product_id}");
+            $productResponse = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token
+            ])->get("http://127.0.0.1:8000/api/obat/{$order->product_id}");
             $data['product'] = $productResponse->json()['data'] ?? null;
 
             // Get the user details (consume)
-            $userResponse = Http::get("{$this->userServiceUrl}/users/{$order->user_id}");
+            $userResponse = Http::get("http://127.0.0.1:5000/users/{$order->user_id}");
             $data['user'] = $userResponse->json()['data'] ?? null;
 
-            return new OrderResource($data, 'Success', 'Order found');
+            return new OrderResource($data, 'berhasil', 'Order found');
         } else {
-            return new OrderResource(null, 'Failed', 'Order not found');
+            return new OrderResource(null, 'gagal', 'Order not found');
         }
     }
 
     public function update(Request $request, $id)
     {
+        $token = $request->bearerToken();
         $order = Order::find($id);
         if (!$order) {
-            return new OrderResource(null, 'Failed', 'Order not found');
+            return new OrderResource(null, 'gagal', 'Order not found');
         }
 
         $validator = Validator::make($request->all(), [
@@ -121,27 +129,33 @@ class OrderController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return new OrderResource(null, 'Failed', $validator->errors());
+            return new OrderResource(null, 'gagal', $validator->errors());
         }
 
         // Get Product Info for price
-        $productResponse = Http::get("{$this->productServiceUrl}/api/obat/{$request->product_id}");
+        $productResponse = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->get("http://127.0.0.1:8000/api/obat/{$request->product_id}");
         $productData = $productResponse->json()['data'] ?? null;
 
         if (!$productData) {
-            return new OrderResource(null, 'Failed', 'Product not found');
+            return new OrderResource(null, 'gagal', 'Product not found');
         }
 
         if ($order->quantity > $request->quantity) {
             $qtt = $order->quantity - $request->quantity;
-            Http::put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
-                'stock' => $productData['stock'] + $qtt,
-            ]);
+            Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token
+            ])->put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
+                        'stock' => $productData['stock'] + $qtt,
+                    ]);
         } else {
             $qtt = $request->quantity - $order->quantity;
-            Http::put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
-                'stock' => $productData['stock'] - $qtt,
-            ]);
+            Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token
+            ])->put("http://127.0.0.1:8000/api/obat/{$request->product_id}", [
+                        'stock' => $productData['stock'] - $qtt,
+                    ]);
         }
 
         $order->update([
@@ -152,29 +166,29 @@ class OrderController extends Controller
         ]);
 
 
-        return new OrderResource($order, 'Success', 'Order updated successfully');
+        return new OrderResource($order, 'berhasil', 'Order updated successfully');
     }
 
     public function updateStatus(Request $request, $id)
     {
         $order = Order::find($id);
         if (!$order) {
-            return new OrderResource(null, 'Failed', 'Order not found');
+            return new OrderResource(null, 'gagal', 'Order not found');
         }
 
         $order->update(['status' => $request->status]);
-        return new OrderResource($order, 'Success', 'Status updated successfully');
+        return new OrderResource($order, 'berhasil', 'Status updated successfully');
     }
 
     public function destroy($id)
     {
         $order = Order::find($id);
         if (!$order) {
-            return new OrderResource(null, 'Failed', 'Order not found');
+            return new OrderResource(null, 'gagal', 'Order not found');
         }
 
         $order->delete();
-        return new OrderResource(null, 'Success', 'Order deleted successfully');
+        return new OrderResource(null, 'berhasil', 'Order deleted successfully');
     }
 
     private function generateOrderCode($user, $product)
